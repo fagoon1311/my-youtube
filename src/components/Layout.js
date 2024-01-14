2f5465b87b1d1aaf05b1a031f07846bb6e0f0e7a
fagoon1311 <fs14smailbox65@gmail.com>
2024-01-14 14:29:53 +0530

    added online offline feature

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Head from './Head'
 import { Outlet } from 'react-router-dom'
+import useOnline from '../utils/useOnline'
 
 const Layout = () => {
+  const isOnline = useOnline()
+  if (!isOnline){
+    return <h1>Seems like you are offline, please check you internet connection 🚨</h1>
+  } 
   return (
    <>
    <Head/>
